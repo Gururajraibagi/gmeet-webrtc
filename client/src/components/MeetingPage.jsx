7084cd1cd141f38491a9ceec9655b87ee8343275
Gururajraibagi <Gururajraibagi7@gmail.com>
2023-07-17 01:36:50 +0530

    fixed bug to load multiple screen

--- client/src/components/MeetingPage.jsx
+++ client/src/components/MeetingPage.jsx
@@ -10,7 +10,7 @@ export function MeetingPage() {
     const [meetingJoined, setMeetingJoined] = useState(false);
     const [videoStream, setVideoStream] = useState();
     const [remoteVideoStream, setRemoteVideoStream] = useState();
-    
+
     const params = useParams();
     const roomId = params.roomId;
 
@@ -22,86 +22,101 @@ export function MeetingPage() {
                 roomId
             })
 
-            window.navigator.mediaDevices.getUserMedia({
-                video: true
-            }).then(async (stream) => {
-                setVideoStream(stream);
-            });
-    
-            s.on("localDescription", async ({description}) => {
-                // Receiving video - 
+            const constraints = {
+                video: true, // Adjust video constraints as needed
+                audio: true, // Adjust audio constraints as needed
+            };
+
+            window.navigator.mediaDevices.getUserMedia(constraints)
+                .then(async (stream) => {
+                    setVideoStream(stream);
+                })
+                .catch((error) => {
+                    console.error("Error accessing media devices:", error);
+                });
+
+            s.on("localDescription", async ({ description }) => {
                 let pc = new RTCPeerConnection({
                     iceServers: [
-                    {
-                        urls: "stun:stun.l.google.com:19302",
-                    },
+                        {
+                            urls: "stun:stun.l.google.com:19302",
+                        },
                     ],
                 });
-                
+
                 pc.setRemoteDescription(description);
                 pc.ontrack = (e) => {
                     setRemoteVideoStream(new MediaStream([e.track]))
                 }
 
-                s.on("iceCandidate", ({candidate}) => {
+                s.on("iceCandidate", ({ candidate }) => {
                     pc.addIceCandidate(candidate)
                 });
 
-                pc.onicecandidate = ({candidate}) => {
-                    s.emit("iceCandidateReply", {candidate});
+                pc.onicecandidate = ({ candidate }) => {
+                    s.emit("iceCandidateReply", { candidate });
                 }
 
                 await pc.setLocalDescription(await pc.createAnswer());
-                s.emit("remoteDescription", {description: pc.localDescription});
+                s.emit("remoteDescription", { description: pc.localDescription });
             })
         });
     }, [])
 
     if (!videoStream) {
-        return <div>
-            Loading...
-        </div>
+        return <div>Loading...</div>
     }
 
     if (!meetingJoined) {
-        return <div style={{minHeight: "100vh",}}>
+        return <div style={{ minHeight: "100vh" }}>
             <CentralizedCard>
                 <div>
                     <Typography textAlign={"center"} variant="h5">
                         Hi welcome to meeting {roomId}.
                     </Typography>
                 </div>
-                <br/><br/>
-                <div style={{display: "flex", justifyContent: "center"}}>
+                <br /><br />
+                <div style={{ display: "flex", justifyContent: "center" }}>
                     <Button onClick={() => {
-                        // sending pc
                         let pc = new RTCPeerConnection({
                             iceServers: [
-                              {
-                                urls: "stun:stun.l.google.com:19302",
-                              },
+                                {
+                                    urls: "stun:stun.l.google.com:19302",
+                                },
                             ],
-                          });
-                        pc.onicecandidate = ({candidate}) => {
-                            socket.emit("iceCandidate", {candidate});
+                        });
+
+                        pc.onicecandidate = ({ candidate }) => {
+                            socket.emit("iceCandidate", { candidate });
                         }
+
                         pc.addTrack(videoStream.getVideoTracks()[0])
+
                         pc.onnegotiationneeded = async () => {
                             try {
                                 await pc.setLocalDescription(await pc.createOffer());
-                                console.log(pc.localDescription);
-                                socket.emit("localDescription", {description: pc.localDescription});
+                                socket.emit("localDescription", { description: pc.localDescription });
                             } catch (err) {
-                                console.error(err);
+                                console.error("Error creating offer:", err);
                             }
                         };
-            
-                        socket.on("remoteDescription", async ({description}) => {
-                            await pc.setRemoteDescription(description);  
+
+                        socket.on("remoteDescription", async ({ description }) => {
+                            try {
+                                if (pc.signalingState === "stable") {
+                                    await pc.setLocalDescription({ type: "rollback" });
+                                }
+
+                                await pc.setRemoteDescription(description);
+                            } catch (error) {
+                                console.error("Error setting remote description:", error);
+                            }
                         });
-                        socket.on("iceCandidateReply", ({candidate}) => {
-                            pc.addIceCandidate(candidate)
+
+                        socket.on("iceCandidateReply", ({ candidate }) => {
+                            pc.addIceCandidate(candidate);
                         });
+
                         setMeetingJoined(true);
                     }} disabled={!socket} variant="contained">
                         Join meeting
@@ -115,8 +130,10 @@ export function MeetingPage() {
         <Grid item xs={12} md={6} lg={4}>
             <Video stream={videoStream} />
         </Grid>
-        <Grid item xs={12} md={6} lg={4}>
-            <Video stream={remoteVideoStream} />
-        </Grid>
+        {remoteVideoStream &&
+            <Grid item xs={12} md={6} lg={4}>
+                <Video stream={remoteVideoStream} />
+            </Grid>
+        }
     </Grid>
 }
